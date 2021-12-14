59279b5822a84ddf4b9640acc495fc2494badd35
gribbib <j.git@motschi.email>
2021-12-14 18:31:22 +0100

    Day 2 - Part 1

---
+++ day2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horizontal = 0, depth = 0;
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            foreach (string line in lines)
+            {
+                var split = line.Split(" ");
+                var x = Convert.ToInt32(split[1]);
+                switch (split[0].ToLower()){
+                    case "forward": horizontal += x;
+                        break;
+                    case "down": depth += x;
+                        break;
+                    case "up": depth -= x;
+                        break;
+                }
+            }
+
+            Console.WriteLine(horizontal * depth);
+        }
+    }
+}
