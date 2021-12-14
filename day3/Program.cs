20baaa59ebd46e0114a98be004c63dd86637645d
gribbib <j.git@motschi.email>
2021-12-14 21:06:51 +0100

    Day 3 - Part 2

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace day3
 {
@@ -37,6 +39,49 @@ namespace day3
             epsilonRate = Convert.ToInt32(binaryString2, 2);
 
             Console.WriteLine(gammaRate * epsilonRate);
+
+            var oxygenGeneratorRating = Convert.ToInt32(compareRecursion(lines.ToList(), true), 2);
+            var co2ScrubberRating = Convert.ToInt32(compareRecursion(lines.ToList(), false), 2);
+            Console.WriteLine(oxygenGeneratorRating * co2ScrubberRating);
+        }
+
+        private static string compareRecursion(IEnumerable<string> test, bool mostCommen, int position = 0){
+            if (test.Count() == 1)
+            {
+                return test.First();
+            }
+
+            if(position >= test.First().Length)
+            {
+                return "0";
+            }
+
+            var countOnes = test.Where(l => l[position] == '1').Count();
+            double countThreshold = test.Count() / 2.0;
+            // Console.WriteLine($"{countThreshold} - {countOnes}");
+            var newPosition = position + 1;
+            if (mostCommen)
+            {
+                if(countOnes >= countThreshold)
+                {          
+                    return compareRecursion(test.Where(l => l[position] == '1'), mostCommen, newPosition);
+                }
+                else
+                {
+                    return compareRecursion(test.Where(l => l[position] == '0'), mostCommen, newPosition);
+                }
+            }
+            else
+            {
+                if(countOnes < countThreshold)
+                {          
+                    return compareRecursion(test.Where(l => l[position] == '1'), mostCommen, newPosition);
+                }
+                else
+                {
+                    return compareRecursion(test.Where(l => l[position] == '0'), mostCommen, newPosition);
+                }
+            }
         }
     }
 }
